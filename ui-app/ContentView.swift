90079c694e06f3c44f59ce3a6b41fb6321e60f9d
TW Webb <twwebb1@me.com>
2024-01-08 16:17:24 -0500

    Initial Commit

---
+++ ui-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ui-app
+//
+//  Created by Theodore Webb on 1/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ui-app/ui_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ui_appApp.swift
+//  ui-app
+//
+//  Created by Theodore Webb on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ui_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
